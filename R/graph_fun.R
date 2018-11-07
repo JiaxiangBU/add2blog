fc299516bf804c3b49fa3a79b94dce3dc2829849
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2018-11-07 12:56:02 +0800

    modify tab for reprex

--- R/graph_fun.R
+++ R/graph_fun.R
@@ -1,15 +1,15 @@
 graph_fun <- function(matrix,fontsize=2.5){
-    matrix %>%
-        as.data.frame %>%
-        rownames_to_column('y') %>%
-        gather(x,text,-y) %>%
-        mutate(x = str_remove_all(x,'V')) %>%
-        mutate_at(vars(x,y),as.integer) %>%
-        ggplot(aes(x=x, y=y, color=factor(text))) +
-        geom_text(aes(label=text, color=factor(text)), family="Helvetica", size=fontsize) +
-        scale_y_reverse() +
-        theme_void() +
-        scale_color_viridis_d(option="magma", begin=0.2, guide="none") +
-        scale_fill_viridis_d(option="magma", begin=0.2, guide="none") +
-        theme(panel.background = element_rect(fill="#000000"))
+  matrix %>%
+    as.data.frame %>%
+    rownames_to_column('y') %>%
+    gather(x,text,-y) %>%
+    mutate(x = str_remove_all(x,'V')) %>%
+    mutate_at(vars(x,y),as.integer) %>%
+    ggplot(aes(x=x, y=y, color=factor(text))) +
+    geom_text(aes(label=text, color=factor(text)), family="Helvetica", size=fontsize) +
+    scale_y_reverse() +
+    theme_void() +
+    scale_color_viridis_d(option="magma", begin=0.2, guide="none") +
+    scale_fill_viridis_d(option="magma", begin=0.2, guide="none") +
+    theme(panel.background = element_rect(fill="#000000"))
 }
--- R/matrix_name.R
+++ R/matrix_name.R
@@ -1,13 +1,13 @@
 matrix_name <-
-    function(x,n=10) {
-        set.seed(123)
-        x %>%
-            str_to_upper() %>%
-            str_split('') %>%
-            .[[1]] %>%
-            sample(size=n*n,replace=T) %>%
-            matrix(
-                nrow = 10
-                ,byrow = T
-            )
-    }
+  function(x,n=10) {
+    set.seed(123)
+    x %>%
+    str_to_upper() %>%
+    str_split('') %>%
+    .[[1]] %>%
+    sample(size=n*n,replace=T) %>%
+    matrix(
+        nrow = 10
+        ,byrow = T
+    )
+  }
